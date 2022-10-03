ac57aa019a820f1e876187a2f95736a947afad7a
who cares <mariaaramirez80@gmail.com>
2022-10-03 10:40:38 -0700

    added generate password function

--- assets/script.js
+++ assets/script.js
@@ -14,6 +14,70 @@ function getRandomItem(list) {
     return list[randomInt(list.length)]
 }
 
+function generatePassword() {
+
+    var UserInput = window.prompt('How long do you want your password to be?')
+
+    var passwordLength = parseInt(UserInput)
+
+    if (isNaN(passwordLength)) {
+        window.alert("That's not a number!")
+        return
+    }
+
+    if (passwordLength < 8 || passwordLength > 128) {
+        window.alert('Password Length must be between 8 and 128 characters')
+        return
+    }
+
+    var userWantsNumbers = window.confirm('Would you like to include numbers in your password?')
+    var userWantsSymbols = window.confirm('Would you like to include symbols in your password?')
+    var userWantsLowercase = window.confirm('Would you like to include lowercase letters in your password?')
+    var userWantsUppercase = window.confirm('Would you like to include uppercase letters in your password?')
+
+    var numberList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    var symbolList = ['!', '@', '#', '$', '%', '^', '&', '*']
+    var lowercaseList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    var uppercaseList = []
+
+    var optionsCart = []
+
+    for (var i = 0; i < lowercaseList.length; i++) {
+        uppercaseList[i] = lowercaseList[i].toUpperCase()
+    }
+
+    if (userWantsNumbers === true) {
+        optionsCart.push(numberList)
+    }
+
+    if (userWantsSymbols === true) {
+        optionsCart.push(symbolList)
+    }
+
+    if (userWantsLowercase === true) {
+        optionsCart.push(lowercaseList)
+    }
+
+    if (userWantsUppercase === true) {
+        optionsCart.push(uppercaseList)
+    }
+
+    if (optionsCart.length === 0){
+        optionsCart.push(lowercaseList)
+    }
+
+    var generatePassword = ""
+
+    for (var i = 0; i< passwordLength; i++) {
+        var randomList= getRandomItem(optionsCart)
+        var randomChar = getRandomItem(randomList)
+        generatePassword += randomChar
+    }
+
+    return generatePassword
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
